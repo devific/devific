cd12fb0bdc8b9d14280c86ed4cc1e68f8b30252e
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2026-03-06 16:15:53 +0530

    Remove meaning section

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -7,7 +7,6 @@ import Process from "../components/Process";
 import WhyUs from "../components/WhyUs";
 import CTA from "../components/CTA";
 import { TrustedBy } from "@/components/TrustedBy";
-import Meaning from "@/components/Meaning";
 
 interface HomeProps {
   onOpenDialog: () => void;
@@ -18,7 +17,6 @@ const Home: React.FC<HomeProps> = ({ onOpenDialog }) => {
     <section className="overflow-x-hidden">
       <Hero onOpenDialog={onOpenDialog} />
       <TrustedBy />
-      <Meaning />
       <About />
       <Projects />
       <Services />
