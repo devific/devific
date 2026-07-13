c85fe4d2ef5aedb2de0ecff8a2518c47a6982bb4
Gavin Pereira <gavineditz@gmail.com>
2026-07-13 22:47:32 +0530

    Fix variable issue

--- scripts/prebuild.ts
+++ scripts/prebuild.ts
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load environment variables
-dotenv.config({ path: path.resolve(__dirname, "../.env.local") });
 dotenv.config();
 
 const HYGRAPH_API_URL = process.env.VITE_HYGRAPH_URL;
